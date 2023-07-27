925d2d119dac2b62e423f5500353f5158bd1fc99
msr1013 <rawalsinghmanish@gmail.com>
2023-07-27 22:05:54 +0545

    Call addBook,displayBook and removeBook methods to add,display and remove books from library objects

--- src/test/java/Library/Main.java
+++ src/test/java/Library/Main.java
@@ -3,15 +3,23 @@ package Library;
 public class Main {
     public static void main(String[] args) {
         Library library = new Library("Manish's Library");
-        Book book1 = new Book("AstroPhysics","Manish Singh Rawal","Twentytwenty");
-        Book book2 = new Book("Delight","Rahul Dev","Forty");
-        Book book3 = new Book("Bonanza","Sikander DePaul","Sixty");
+        Book book1 = new Book("AstroPhysics", "Manish Singh Rawal", "Twentytwenty");
+        Book book2 = new Book("Delight", "Rahul Dev", "Forty");
+        Book book3 = new Book("Bonanza", "Sikander DePaul", "Sixty");
 
         library.addBook(book1);
         library.addBook(book2);
         library.addBook(book3);
 
         library.displayBook();
+
+        Book s = library.findBook("Delight");
+        System.out.println("The details of book found is: " + s);
+
+        library.removeBook(book2);
+        System.out.println("The updateed book list is");
+        library.displayBook();
+
 
 
     }
